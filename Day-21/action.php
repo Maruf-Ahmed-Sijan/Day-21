e2324e5f994016e9cc9f1a8c3a2ace0c63317011
Maruf-Ahmed-Sijan <98020643+Maruf-Ahmed-Sijan@users.noreply.github.com>
2022-02-13 17:20:36 +0600

    Add files via upload
    Day-21

---
+++ Day-21/action.php
@@ -0,0 +1,17 @@
+<?php
+require_once 'vendor/autoload.php';
+use App\Classes\Calculator;
+
+if (isset($_GET['pages']))
+{
+    if ($_GET['pages'] == 'home')
+    {
+        include "pages/home.php";
+    }
+}
+elseif (isset($_POST['btn']))
+{
+    $calculator = new Calculator($_POST);
+    $result = $calculator->index();
+    include 'pages/home.php';
+}
